bba7a0efc482aac96aaff46fab9d8d1b847f1418
Thiru <vijayofficial580@gmail.com>
2022-04-06 10:35:35 +0530

    first

---
+++ QuotientAndRemainder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace QoutientAndRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+        
+                Console.Write("Enter the number : ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nEnter the divisor :");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+                int remainder;
+                int quotient;
+
+                //Compute
+                quotient = number / divisor;
+                remainder = number % divisor;
+
+                //Result
+                Console.WriteLine("The quotient is {0}", quotient);
+                Console.WriteLine("The remainder is {0}", remainder);
+
+            
+        }
+
+
+    }
+}
+
