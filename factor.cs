20bddb76a3dec0f98dd1d56c69a6767e5bf8933d
Thiru <vijayofficial580@gmail.com>
2022-04-06 08:36:50 +0530

    factor

---
+++ factor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Day_5
+{
+    class Factor
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Enter positive number : ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                int originalNumber = 1;
+
+                if (number < 0)
+                {
+                    Console.WriteLine("Invalid Number!");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    for (int count = 2; number > 1; count++)
+                    {
+                        while (number % count == 0)
+                        {
+                            Console.Write("{0} ", count);
+                            number /= count;
+                            originalNumber *= count;
+                        }
+                    }
+                    Console.WriteLine("\nOriginal Number after prime factorization : {0}", originalNumber);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+    
+}
