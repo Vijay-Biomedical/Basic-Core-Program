24a88965eb7b22fde424893e7ca0614db48839de
Thiru <vijayofficial580@gmail.com>
2022-04-06 10:43:11 +0530

    Swapnumbeer

---
+++ Swapnumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SwapNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the number 1 : ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the number 2 : ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int originalNumber1 = number1;
+            int originalNumber2 = number2;
+            int temp;
+
+            //swap logic
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+
+            Console.WriteLine("Value of number 1 before swapping : {0}", originalNumber1);
+            Console.WriteLine("Value of number 2 before swapping : {0}", originalNumber2);
+
+            Console.WriteLine("Value of number 1 after swapping : {0}", number1);
+            Console.WriteLine("Value of number 2 after swapping : {0}", number2);
+
+        }
+    }
+}
+
+        
