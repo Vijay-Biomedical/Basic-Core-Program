664a2076fd41447fdb69edc06535da19a8288331
Thiru <vijayofficial580@gmail.com>
2022-04-06 11:06:07 +0530

    addd

---
+++ vowel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oddandeven
+{
+    internal class VowelOrConsonant
+    {
+        public static void CheckAlphabet()
+        {
+            char character;
+            Console.WriteLine("Enter the character : ");
+            character = Convert.ToChar(Console.ReadLine());
+
+            if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' ||
+                character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'Y')
+            {
+                Console.WriteLine("{0} is vowel.", character);
+            }
+            else
+            {
+                Console.WriteLine("{0} is consonant.", character);
+            }
+        }
+    }
+}
+
