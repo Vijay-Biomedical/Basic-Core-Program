68fe26c48ac126217cbb82e75d7a3c3009fd691f
Thiru <vijayofficial580@gmail.com>
2022-04-06 08:13:44 +0530

    Harmonic number

---
+++ Harmonic_number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Harmonnic_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Nth number : ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            double sumOfHarmonicNumber = 0.0;
+            for (int i = 1; i <= number; i++)
+            {
+                sumOfHarmonicNumber = 1 + (1 / number);
+                Console.Write("1/{0} ", i);
+            }
+        }
+    }
+}
